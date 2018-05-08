c896c2fc1a9a045f94eaf70ffe4a1d152bc7fd3f
Michal Frystacky <Michal.Frystacky@alaska.gov>
2018-05-08 15:05:36 -0800

    Feat: Improved lint config and docs

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// Guide to eslint config: https://eslint.org/docs/user-guide/configuring
+//
+// this js eslintrc is prioritized over other eslintrc formats
+// ref: https://eslint.org/docs/user-guide/configuring#configuration-file-formats
+// ref: https://medium.com/stephenkoo/how-to-set-up-create-react-app-redux-react-router-redux-thunk-prettier-scss-airbnb-eslint-dda0bba5616a
+//
+// This config brings prettier for formatting and eslint for best practices
+//  together in harmony. (ref: https://stackoverflow.com/q/46201647/1771644)
+module.export = {
+  "parser": "babel-eslint",
+  "extends": ["prettier", "airbnb"],
+  "plugins": ["react", "jsx-a11y", "import", "prettier"],
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  },
+
+}
